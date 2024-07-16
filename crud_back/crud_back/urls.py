08e4baa537e3ad7a066529b13c97d5ca28ab8c1a
João Manoel <jmaoliveira.cic@uesc.br>
2024-07-15 21:22:38 -0300

    feat: Add initial Django API structure

---
+++ crud_back/crud_back/urls.py
@@ -0,0 +1,8 @@
+from api import urls
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(urls)),
+]
