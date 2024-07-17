cdccbf53c1231ef0855f48ea43637f3f5eaea12e
João Manoel <jmaoliveira.cic@uesc.br>
2024-07-16 22:57:19 -0300

    feat: Adicionar modelo de usuário e serializador

--- crud_back/api/models.py
+++ crud_back/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(User):
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        return self.username
